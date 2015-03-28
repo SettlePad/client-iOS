33bc59f0a347c961bcca6570377881220cece9e2
Rob Everhardt <rob@everhardt.nl>
2015-03-28 23:23:21 +0100

    Actions in IdentifiersMenu

--- UOless/IdentifiersViewController.swift
+++ UOless/IdentifiersViewController.swift
@@ -6,6 +6,8 @@
 //  Copyright (c) 2015 UOless. All rights reserved.
 //
 
+// See http://nshipster.com/uialertcontroller/
+
 import UIKit
 
 class IdentifiersViewController: UITableViewController {
@@ -56,30 +58,60 @@ class IdentifiersViewController: UITableViewController {
         return cell
     }
     
-    //TODO: implement deletion edit
-    //TODO: implement UIAlertController, see http://nshipster.com/uialertcontroller/
     
     
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return NO if you do not want the specified item to be editable.
-        return true
+        if user!.userIdentifiers.count > 1 { //TODO: change to 1
+            return true
+        } else {
+            return false
+        }
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
+        /*if editingStyle == .Delete {
             // Delete the row from the data source
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        } */   
     }
-    */
+    
+    override func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [AnyObject]?  {
+        let identifier = user!.userIdentifiers[indexPath.row]
+        
+        var deleteAction = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "Delete" , handler: { (action:UITableViewRowAction!, indexPath:NSIndexPath!) -> Void in
+            self.deleteIdentifier(identifier, tableView: tableView, indexPath: indexPath)
+        })
+        
+        deleteAction.backgroundColor = Colors.danger.textToUIColor()
+        return [deleteAction]
+    }
+    
+    func deleteIdentifier (identifier: UserIdentifier, tableView: UITableView, indexPath: NSIndexPath) {
+        let alertController = UIAlertController(title: "Are you sure?", message: "Do you really want to delete "+identifier.identifier+"?", preferredStyle: .Alert)
 
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (action) in
+            tableView.setEditing(false, animated: true)
+        }
+        alertController.addAction(cancelAction)
+        
+        let destroyAction = UIAlertAction(title: "Delete", style: .Destructive) { (action) in
+            //TODO: implement delete POST in User
+            tableView.setEditing(false, animated: true)
+        }
+        alertController.addAction(destroyAction)
+        
+        self.presentViewController(alertController, animated: true) {
+            // ...
+        }
+    }
+    
     /*
     // Override to support rearranging the table view.
     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
@@ -104,5 +136,155 @@ class IdentifiersViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        let identifier = user!.userIdentifiers[indexPath.row]
+        let cell = tableView.cellForRowAtIndexPath(indexPath)
+        
+        if (identifier.verified && user!.userIdentifiers.count == 0) {
+            //only one option: change password
+            changePasswordForm()
+        } else {
+            //show Action sheet
 
+            let actionSheetController: UIAlertController = UIAlertController(title: nil, message: nil, preferredStyle: .ActionSheet)
+            
+            let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .Cancel) { action -> Void in
+                //Just dismiss the action sheet
+            }
+            actionSheetController.addAction(cancelAction)
+            
+            let changePasswordAction: UIAlertAction = UIAlertAction(title: "Change password", style: .Default) { action -> Void in
+                self.changePasswordForm()
+            }
+            actionSheetController.addAction(changePasswordAction)
+            
+            if (identifier.verified == false) {
+                let verifyAction: UIAlertAction = UIAlertAction(title: "Resend verification email", style: .Default) { action -> Void in
+                    //TODO: outlet POST
+                }
+                actionSheetController.addAction(verifyAction)
+            }
+            
+            if (user!.userIdentifiers.count > 0) {
+                let verifyAction: UIAlertAction = UIAlertAction(title: "Delete", style: .Destructive) { action -> Void in
+                    let identifier = user!.userIdentifiers[indexPath.row]
+                    self.deleteIdentifier (identifier, tableView: tableView, indexPath: indexPath)
+                }
+                actionSheetController.addAction(verifyAction)
+            }
+
+            //We need to provide a popover sourceView when using it on iPad
+            actionSheetController.popoverPresentationController?.sourceView = cell?.contentView
+            actionSheetController.popoverPresentationController?.permittedArrowDirections = UIPopoverArrowDirection.Up | UIPopoverArrowDirection.Down
+            actionSheetController.popoverPresentationController?.sourceRect = CGRectMake(cell!.frame.width / 2, cell!.frame.height,0,0)
+
+            //Present the AlertController
+            self.presentViewController(actionSheetController, animated: true, completion: nil)
+        }
+    }
+    
+    func changePasswordForm() {
+        //show change password form
+        let alertController = UIAlertController(title: "Change password", message: "Enter your new password twice to change it", preferredStyle: .Alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (action) in }
+        
+        let changeAction = UIAlertAction(title: "Change", style: .Destructive) { (action) in
+            //TODO: make change pwd endpoint
+        }
+        changeAction.enabled = false
+        
+        alertController.addTextFieldWithConfigurationHandler { (textField) in
+            textField.placeholder = "Password"
+            textField.secureTextEntry = true
+            textField.addTarget(self, action: "changePasswordFormTextChanged:", forControlEvents: .EditingChanged)
+        }
+        
+        
+        alertController.addTextFieldWithConfigurationHandler { (textField) in
+            textField.placeholder = "Password (again)"
+            textField.secureTextEntry = true
+            textField.addTarget(self, action: "changePasswordFormTextChanged:", forControlEvents: .EditingChanged)
+        }
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(changeAction)
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func changePasswordFormTextChanged(sender:AnyObject) {
+        //get a handler to the UIAlertController
+        let tf = sender as UITextField
+        var resp : UIResponder = tf
+        while !(resp is UIAlertController) { resp = resp.nextResponder()! }
+        let alertController = resp as UIAlertController
+        
+        let firstPasswordTextField = alertController.textFields![0] as UITextField
+        let secondPasswordTextField = alertController.textFields![1] as UITextField
+        
+        (alertController.actions[1] as UIAlertAction).enabled = (
+                firstPasswordTextField.text != "" &&
+                firstPasswordTextField.text == secondPasswordTextField.text
+        )
+    }
+    
+
+    @IBAction func newIdentifierAction(sender: AnyObject) {
+        //show new identifier form
+        let alertController = UIAlertController(title: "New identifier", message: nil, preferredStyle: .Alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel) { (action) in
+
+        }
+        
+        let addAction = UIAlertAction(title: "Add", style: .Default) { (action) in
+            //TODO: make add new identifier endpoint
+        }
+        addAction.enabled = false
+
+        alertController.addTextFieldWithConfigurationHandler { (textField) in
+            textField.placeholder = "Email address"
+            textField.secureTextEntry = false
+            textField.addTarget(self, action: "newIdentifierFormTextChanged:", forControlEvents: .EditingChanged)
+        }
+        
+        alertController.addTextFieldWithConfigurationHandler { (textField) in
+            textField.placeholder = "Password"
+            textField.secureTextEntry = true
+            textField.addTarget(self, action: "newIdentifierFormTextChanged:", forControlEvents: .EditingChanged)
+        }
+        
+        alertController.addTextFieldWithConfigurationHandler { (textField) in
+            textField.placeholder = "Password (again)"
+            textField.secureTextEntry = true
+            
+            textField.addTarget(self, action: "newIdentifierFormTextChanged:", forControlEvents: .EditingChanged)
+        }
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(addAction)
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func newIdentifierFormTextChanged(sender:AnyObject) {
+        //get a handler to the UIAlertController
+        let tf = sender as UITextField
+        var resp : UIResponder = tf
+        while !(resp is UIAlertController) { resp = resp.nextResponder()! }
+        let alertController = resp as UIAlertController
+        
+        let emailTextField = alertController.textFields![0] as UITextField
+        let firstPasswordTextField = alertController.textFields![1] as UITextField
+        let secondPasswordTextField = alertController.textFields![2] as UITextField
+        
+        (alertController.actions[1] as UIAlertAction).enabled = (
+            emailTextField.text.isEmail() &&
+            firstPasswordTextField.text != "" &&
+            firstPasswordTextField.text == secondPasswordTextField.text
+        )
+    }
 }
